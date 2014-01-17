2b02b91ad655c76715139b2153ddb0e4d5e5d4c2
user-daw-zayas <javier.perezarteaga@educa.madrid.org>
2014-01-17 13:55:52 +0100

    17/01/2014

--- php/registro.php
+++ php/registro.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<?php
+
+echo '<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -6,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Galeria de coches</title>
+    <title>Registro</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
@@ -17,7 +19,7 @@
   </head>
 
   <body>
-
+   <form role="form" method="POST" action="connectdb.php">
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
@@ -39,14 +41,13 @@
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Galeria <b class="caret"></b></a>
               <ul class="dropdown-menu">
                 <li><a href="portfolio-1-col.html">Proyectos</a></li>
-                <li class="active"><a href="#">Galeria de coches</a></li>
+                <li><a href="portfolio-2-col.html">Galeria de coches</a></li>
               </ul>
             </li>
             <li class="dropdown">
               <a href="#">Foro</a>
             </li>
-            <li class="dropdown">
-              <a href="registro.php">Registro</a>
+            <li><a href="registro.php">Registro</a>
             </li>
             <li><a href="#">LogIn</a></li>
           </ul>
@@ -59,77 +60,45 @@
       <div class="row">
 
         <div class="col-lg-12">
-          <h1 class="page-header">Galeria de coches <small>Échale un vistazo a lo último.</small></h1>
-          <ol class="breadcrumb">
-            <li><a href="index.html">Home</a></li>
-            <li class="active">Galeria de coches</li>
-          </ol>
+          <h1 class="page-header">Registro <small>Rellena el formulario</small></h1>
         </div>
 
       </div>
 
       <div class="row">
 
-        <div class="col-md-6 portfolio-item">
-          <a href="portfolio-item.html"><img class="img-responsive" src="http://placehold.it/750x450"></a>
-          
-        </div>
-
-        <div class="col-md-6 portfolio-item">
-          <a href="portfolio-item.html"><img class="img-responsive" src="http://placehold.it/750x450"></a>
-         
-        </div>
-
-      </div>
-
-      <div class="row">
-
-        <div class="col-md-6 portfolio-item">
-          <a href="portfolio-item.html"><img class="img-responsive" src="http://placehold.it/750x450"></a>
-          
-        </div>
-
-        <div class="col-md-6 portfolio-item">
-          <a href="portfolio-item.html"><img class="img-responsive" src="http://placehold.it/750x450"></a>
-          
-        </div>
-
-      </div>
-
-      <div class="row">
-
-        <div class="col-md-6 portfolio-item">
-          <a href="portfolio-item.html"><img class="img-responsive" src="http://placehold.it/750x450"></a>
-          
-        </div>
-
-        <div class="col-md-6 portfolio-item">
-          <a href="portfolio-item.html"><img class="img-responsive" src="http://placehold.it/750x450"></a>
-          
-        </div>
-
-      </div>
-
-      <hr>
-
-      <div class="row text-center">
-        
         <div class="col-lg-12">
-          <ul class="pagination">
-            <li><a href="#">&laquo;</a></li>
-            <li class="active"><a href="#">1</a></li>
-            <li><a href="#">2</a></li>
-            <li><a href="#">3</a></li>
-            <li><a href="#">4</a></li>
-            <li><a href="#">5</a></li>
-            <li><a href="#">&raquo;</a></li>
-          </ul>        
+              <div class="row">
+                <div class="form-group col-lg-4">
+                  <label for="input1">Nombre*</label>
+                  <input type="text" name="nombre" class="form-control" id="input1" placeholder="nombre de usuario">
+                </div>
+                <div class="form-group col-lg-4">
+                  <label for="input2">Contraseña*</label>
+                  <input type="password" name="password" class="form-control" id="input2">
+                </div>
+                <div class="form-group col-lg-4">
+                  <label for="input3">Direccion E-Mail*</label>
+                  <input type="email" name="email" class="form-control" id="input3">
+                </div>
+                <div class="clearfix"></div>
+                <div class="form-group col-lg-12">
+                  <label for="input4">Mensaje</label>
+                  <textarea name="mensaje" class="form-control" rows="6" id="input4" placeholder="Escribe aqui tu mensaje"></textarea>
+                </div>
+                <div class="form-group col-lg-12">
+                  <input type="hidden" name="save" value="contact">
+                  <button type="submit" class="btn btn-primary">Enviar</button>
+                  <br>Los campos con * son obligatorios.
+                </div>
+              </div>
+            </form>
         </div>
 
       </div>
 
     </div><!-- /.container -->
-
+    </form>
     <div class="container">
 
       <hr>
@@ -150,4 +119,6 @@
     <script src="js/modern-business.js"></script>
 
   </body>
-</html>
+</html>';
+
+?>
