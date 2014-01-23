978a73340ed15eade6ce72bdf11d5f0b060cffb1
user-daw-zayas <javier.perezarteaga@educa.madrid.org>
2014-01-23 10:38:38 +0100

    23/01/2014

---
+++ php/servidor/mailmailmail.php
@@ -0,0 +1,43 @@
+<?php
+
+require "../lib/PHPMailerAutoload.php";
+include("../lib/class.phpmailer.php");
+include("../lib/class.smtp.php");
+
+function send_mail($mail_register, $cuerpo) {
+    $mail = new PHPMailer();
+
+    $mail->IsSMTP();
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = "ssl";
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 465;
+    $mail->Username = "webpaintitblack@gmail.com";
+    $mail->Password = "paintitblack123456";
+
+
+    $mail->From = "webpaintitblack@gmail.com";
+    $mail->FromName = "PaintItBlack";
+
+    $mail->addAddress($mail_register);
+    $mail->WordWrap = 50;
+    $mail->isHTML(true);
+    $mail->Subject = "Mensaje";
+    $mail->Body = $cuerpo;
+    $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+
+    if (!$mail->send()) {
+        echo "Message could not be sent.";
+        echo "Mailer Error: " . $mail->ErrorInfo;
+        return false;
+    }
+    return true;
+}
+$mail_register=$_POST['email'];
+$cuerpo="Mensaje del usuario: <b>$_POST[name]</b> con email: $_POST[email].".$_POST['message'];
+if(send_mail($mail_register,$cuerpo)){
+    echo "Se ha enviado correctamente";
+    echo "<a href=index.html>Volver</a>";
+}
+
+?>
