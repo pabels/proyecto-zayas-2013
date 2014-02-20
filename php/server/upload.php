d09fc1c847fd4956fccb5459899fe68949a47f5d
user-daw-zayas <javier.perezarteaga@educa.madrid.org>
2014-02-20 13:56:27 +0100

    20/02/2014

--- php/server/upload.php
+++ php/server/upload.php
@@ -1,7 +1,7 @@
 <?php
 $uploadedfileload="true";
+$msg="";
 $uploadedfile_size=$_FILES['uploadedfile']['size'];
-echo $_FILES['uploadedfile']['name'];
 if ($_FILES['uploadedfile']['size']>20000000){
 	$msg=$msg."El archivo es mayor que 200KB, debes reduzcirlo antes de subirlo<BR>";
 	$uploadedfileload="false";
@@ -13,16 +13,28 @@ if (!($_FILES['uploadedfile']['type'] =="image/jpeg" OR $_FILES['uploadedfile'][
 }
 
 $file_name=$_FILES['uploadedfile']['name'];
-$add="coches/$file_name";
+$add="uploaded/$file_name";
 if($uploadedfileload=="true"){
 
 	if(move_uploaded_file ($_FILES['uploadedfile']['tmp_name'], $add)){
+		echo $_FILES['uploadedfile']['name'].'<br>';
 		echo " Ha sido subido satisfactoriamente";
+		define("Servidor","localhost");
+        define("User","root");
+        define("Pass","");
+        $conexion=mysql_connect(Servidor,User,Pass) or die ("Error, algo no ha ido bien");
+        mysql_select_db("black",$conexion);
+        $query="insert into coches values('$file_name',0)";
+        mysql_query($query,$conexion);
+        mysql_close($conexion);
+		 header('Refresh: 3; ../client/tools.html');
 	}else{
 		echo "Error al subir el archivo";
 	}
 
 }else{
-	echo $msg;
+	echo $msg.'<br>';
+	echo "<a href=../client/tools.html>Atras</a>";
 }
+
 ?>
